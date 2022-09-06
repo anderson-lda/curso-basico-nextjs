77e433ba8723105f033b27fe792c5cccd7f935aa
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-09-06 15:26:39 -0300

    added sass

---
+++ README.md
@@ -0,0 +1,5 @@
+ - Dependências de desenvolvimento (começou como JS):
+npm install -D typescript @types/react @types/node
+ 
+ - Estilização:
+npm install sass
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
+import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1>Dev</h1>
+    <h1 className={styles.title}>Dev</h1>
   )
 }
