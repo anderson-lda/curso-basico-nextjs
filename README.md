9c80cb97a3a243a1caea746aa19876c0383d4f7b
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-09-09 10:06:47 -0300

    client side rendering

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ https://fonts.google.com/specimen/Roboto
 
  - detalhes da personalização do document obtido em:
 https://nextjs.org/docs/advanced-features/custom-document
+
+ - instalação de api fake (não de forma global):
+ npx json-server api.json -p 3333 -w
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
-import styles from '../styles/home.module.scss'
+//import styles from '../styles/home.module.scss'
+
+import { useEffect, useState } from "react";
+
+interface Post {
+  id: string;
+  title: string;
+}
 
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/posts').then((response) => { //fetch é alternativa nativa ao axios
+      response.json().then((data) => { //then é usado porque são chamadas assíncronas e async não é atribuível
+        setPosts(data)
+      })
+    })
+  }, []);
+
   return (
-    <h1>Dev</h1>
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post)=>(
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
