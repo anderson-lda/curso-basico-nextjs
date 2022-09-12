f817d6b45955d80dd142ff9c1718dbe42b6b4de2
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-09-12 09:17:09 -0300

    created static page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,35 @@
-export default function Posts() {
+import { GetStaticProps } from "next";
+
+interface Post {
+  id: string;
+  title: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({posts}:PostsProps) {
     return (
-      <h1>Posts </h1>
+      <div>
+        <h1>Listagem de Posts</h1>
+        <ul>
+        {posts.map((post)=>(
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+      </div>
     )
   }
   
+  //página estática só é criada no npm run build
+  export const getStaticProps: GetStaticProps<PostsProps> = async () => {
+    const response = await fetch('http://localhost:3333/posts');
+    const posts = await response.json();
+    return{
+      props:{
+        posts,
+      },
+      revalidate: 1000, //em segundos, tempo em que os HTMLs são recriados
+    }
+  }
