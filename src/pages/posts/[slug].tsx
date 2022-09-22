12673c24003b92d349071893836b646847af9721
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-09-22 10:11:58 -0300

    done post page

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,83 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { useRouter } from "next/router"
+import Prismic from '@prismicio/client'
+import {RichText} from 'prismic-dom'
+import { getPrismicClient } from "../../services/prismic";
+import SEO from "../../components/SEO";
+import styles from './post.module.scss'
+
+interface PostProps {
+    post: {
+        slug: string;
+        title: string;
+        content: string;
+        updateAt: string;
+    }
+}
+
+export default function Post({post}:PostProps) {
+    const router = useRouter();
+
+    if(router.isFallback){ //se a página estática não existir, será criada no primeiro acesso e aparecerá o parágrafo abaixo
+        return <p>Loading...</p>
+    }
+
+    return (
+        <>
+        <SEO title="Post" />
+        <main className={styles.container}>
+          <article className={styles.post}>
+            <h1>{post.title}</h1>
+            <time>{post.updateAt}</time>
+            <div className={styles.content} dangerouslySetInnerHTML={{__html:post.content}} />
+          </article>
+        </main>
+        </>
+    )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    //comandos abaixo permitem criar páginas estáticas no momento do build
+    /*const response = await fetch('http://localhost:3333/posts');
+    const posts = await response.json();
+
+    const paths = posts.map(post => {
+        return {
+            params: {id: String(post.id)}
+        }
+    })*/
+
+    return {
+        //paths,
+        paths: [],
+        fallback: true, //como true cria página a cada acesso
+    };
+}
+
+//cria a página estática
+export const getStaticProps: GetStaticProps = async (context) => {
+    const {slug} = context.params;
+    const prismic = getPrismicClient();
+
+    const response = await prismic.getByUID('post',String(slug),{});
+
+    //formatação é feita aqui no Node para que não seja refeita cada vez que for chamada no browser (caso fosse dentro do return da página)
+    const post = {
+        slug,
+        title: RichText.asText(response.data.title),
+        content: RichText.asText(response.data.content),
+        updateAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR',{
+          day:'2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+
+
+    return{
+      props:{
+        post,
+      },
+      revalidate: 60*60*12, //em segundos, tempo em que os HTMLs são recriados caso próximo acesso
+    }
+  }
